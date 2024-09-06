f7142d15b9466418d0d64d1320cbbbcb2d5ae20a
Danielle Gouveia <danielle.vieira9@etec.sp.gov.br>
2024-09-06 15:06:32 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Clear();
+
+Console.Write("Contando até dez:");
+
+Thread.Seep(1000);
+Console.Write(" 0");
+
+Thread.Seep(1000);
+Console.Write(" 1");
+
+ Thread.Seep(1000);
+Console.Write(" 2");
+
+ Thread.Seep(1000);
+Console.Write(" 3");
+
+ Thread.Seep(1000);
+Console.Write(" 4");
+
+Thread.Seep(1000);
+Console.Write(" 5");
+
+Thread.Seep(1000);
+Console.Write(" 6");
+
+Thread.Seep(1000);
+Console.Write(" 7");
+
+Thread.Seep(1000);
+Console.Write(" 8");
+
+Thread.Seep(1000);
+Console.Write(" 9");
+
+Thread.Seep(1000);
+Console.Write(" 10");
